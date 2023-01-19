44ac1264807c0eacd0370f2b8641cba01a474f6c
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-19 22:03:51 +0100

    Update n.5 date format

--- query_db-university.sql
+++ query_db-university.sql
@@ -29,7 +29,7 @@ WHERE `year` = 1 AND `period` = 'I semestre';
 
 SELECT * 
 FROM `exams`
-WHERE `hour` > '14:00:00' AND `date`= '2020/06/20';
+WHERE `hour` > '14:00:00' AND `date`= '2020-06-20';
 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
