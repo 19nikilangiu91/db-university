72c1ebcaf755bbc0b59322779e63c90cb779fbfc
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-20 17:09:06 +0100

    Update Bonus 5 and 6

--- query_join.sql
+++ query_join.sql
@@ -39,3 +39,29 @@ FROM students
         ON degrees.department_id = departments.id
 ORDER BY students.surname, students.name ASC;
 
+-- BONUS
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT degrees.name, course_teacher.course_id, teachers.*
+FROM degrees    
+    JOIN courses        
+        ON degrees.id = courses.degree_id    
+    JOIN course_teacher        
+        ON courses.id = course_teacher.course_id    
+    JOIN teachers        
+        ON course_teacher.teacher_id = teachers.id 
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica 
+
+SELECT teachers.*, departments.name
+FROM teachers    
+    JOIN course_teacher        
+        ON teachers.id = course_teacher.teacher_id    
+    JOIN courses        
+        ON course_teacher.course_id = courses.id    
+    JOIN degrees        
+        ON courses.degree_id = degrees.id   
+    JOIN departments        
+        ON degrees.department_id = departments.id
+WHERE departments.name LIKE "Dipartimento di Matematica";
